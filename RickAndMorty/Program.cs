fa5cfba84479bc875302a75f3be5547bc813c56e
joseluisgz98 <joseluis_cadiz98@hotmail.com>
2023-12-04 15:43:12 +0100

    Agregar archivos de proyecto.

---
+++ RickAndMorty/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+    public class Personajes
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string[] episode { get; set; }
+    }
+
+    class Program
+    {
+        static HttpClient rickymorty = new HttpClient();
+
+        static async Task Main()
+        {
+            rickymorty.BaseAddress = new Uri("https://rickandmortyapi.com/api");
+
+            string nombre = "Summer Smith";
+            Personajes personaje = await GetPersonajeAsync(nombre);
+
+            if (personaje != null)
+            {
+                Console.WriteLine($"Episodios con {nombre}:");
+                foreach (var episodeUrl in personaje.episode)
+                {
+                    string episodeNumber = episodeUrl.Substring(episodeUrl.LastIndexOf('/') + 1);
+                    Console.WriteLine($"- Episodio {episodeNumber}");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static async Task<Personajes> GetPersonajeAsync(string nombre)
+        {
+            Personajes personaje = null;
+            HttpResponseMessage response = await rickymorty.GetAsync($"character/?name={nombre}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                personaje = await response.Content.ReadAsAsync<Personajes>();
+            }
+
+            return personaje;
+        }
+    }
+}
+
